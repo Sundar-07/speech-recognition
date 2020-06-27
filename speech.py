65e7f7ab702392ca683967da900812170fc2bff3
Sundararajan <43498008+Sundar-07@users.noreply.github.com>
2020-06-28 02:23:21 +0530

    main program

---
+++ speech.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import time
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+harvard = sr.AudioFile('C:/Users/HP/Downloads/New folder/harvard.wav')
+with harvard as source:
+    time.sleep(1)
+    print('Say Something!!')
+    audio = r.listen(source)
+    try:
+        voice = r.recognize_google(audio)
+        print(voice)
+        f = open('C:/Users/HP/Downloads/New folder/output.txt','w')
+
+        f.write('This is your Words:\n {}'.format(voice))
+        f.close()
+    except sr.UnknownValueError:
+        print("Sorry! I dont get it! Please repeat")
+    except sr.RequestError:
+        print('Sorry my speech service is down!')
+
